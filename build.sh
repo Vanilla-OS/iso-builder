0ed96dd18dfb0a4392f2c8368839e2e2b80fe39a
Mirko Brombin <send@mirko.pm>
2023-05-17 21:15:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -74,7 +74,7 @@ build () {
   OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
   mkdir -p "$OUTPUT_DIR"
   FNAME="VanillaOS-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
-  mv "$BASE_DIR/tmp/*.iso" "$OUTPUT_DIR/${FNAME}.iso"
+  mv $BASE_DIR/tmp/*.iso "$OUTPUT_DIR/${FNAME}.iso"
 
   # cd into output to so {FNAME}.sha256.txt only
   # includes the filename and not the path to
